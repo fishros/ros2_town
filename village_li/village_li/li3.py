b440e8235f6bbfe30e837dcbef4bfb27d9d31b5b
aiotrobot <sangxin2014@sina.com>
2021-08-15 13:35:58 +0800

    完成李三借钱吃麻辣烫

--- village_li/village_li/li3.py
+++ village_li/village_li/li3.py
@@ -11,22 +11,49 @@ class Li3Node(Node):
     """
     def __init__(self):
         super().__init__("li3")
+        #打印一句话
         self.get_logger().info("大家好，我是李三，李四他哥，我可以白嫖李四的小说！")
+        #创建一个订阅客户端，用于订阅小说
         self.sub_ = self.create_subscription(String,"sexy_girl", self.recv_callback,10)
-        self.borrow_money_timer_ = self.create_timer(6,self.borrow_money_eat)
+        #创建一个服务客户端
+        self.borrow_money_client_ = self.create_client(BorrowMoney, "borrow_money")
         
     def borrow_money_eat(self):
-        self.get_logger().info("找我哥借钱吃麻辣烫喽")
-        self.borrow_money_client_ = self.create_client(BorrowMoney, "borrow_money")
+        """
+        借钱吃麻辣烫函数
+        """
+        #打印一句话
+        self.get_logger().info("找我弟借钱吃麻辣烫喽")
+        #等待服务启动，每1s检查一次，如果服务没有启动，则一直循环
+        while not self.borrow_money_client_.wait_for_service(1.0):
+            self.get_logger().warn("我弟不在线，我再等等。")
+        # 构建请求内容
         request = BorrowMoney.Request()
+        #将当前节点名称作为借钱者姓名
         request.name.data = self.get_name()
+        #借钱金额5元
         request.money.data = 5
-        response = self.borrow_money_client_.call_async(request)
+        #发送异步借钱请求，借钱成功后就调用borrow_respoonse_callback()函数
+        self.borrow_money_client_.call_async(request).add_done_callback(self.borrow_respoonse_callback)
 
+    def borrow_respoonse_callback(self,response):
+        """
+        借钱结果回调
+        """
+        # 打印一下信息
+        result = response.result()
+        if result.success.data == True:
+            self.get_logger().info("果然是亲弟弟，借到%d,吃麻辣烫去了" % result.money.data)
+        else:
+            self.get_logger().info("害，连十块钱都不借")
 
     def recv_callback(self,novel):
+        """
+        小说接收结果回调
+        """
+        # 打印接收小说结果
         self.get_logger().info('李三：我已经收到了：%s' % novel.data)
-        self.get_logger().info("当前进程中线程数量：%d" % len(threading.enumerate()))
+
 
 def main(args=None):
     """
@@ -34,6 +61,6 @@ def main(args=None):
     """
     rclpy.init(args=args) # 初始化rclpy
     node = Li3Node()  # 新建一个节点
-    
+    node.borrow_money_eat() #李四借钱
     rclpy.spin(node) # 保持节点运行，检测是否收到退出指令（Ctrl+C）
     rclpy.shutdown() # rcl关闭
