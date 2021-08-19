57fa89ec1a763a308a0561a9874c12b55f189d61
aiotrobot <sangxin2014@sina.com>
2021-08-19 04:59:56 -0700

    add zhang3 code

--- village_zhang/src/zhang3.cpp
+++ village_zhang/src/zhang3.cpp
@@ -1,8 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "village_interfaces/srv/sell_novel.hpp"
 
-using std::placeholders::_1;
-using std::placeholders::_2;
 
 /*
     创建一个类节点，名字叫做Zhang3Node,继承自Node.
@@ -17,9 +15,19 @@ public:
         auto sub1_opt = rclcpp::SubscriptionOptions();
         sub1_opt.callback_group = callback_group_subscriber1_;
         // 打印一句自我介绍
-        RCLCPP_INFO(this->get_logger(), "大家好，我是单身汉王二.");
+        RCLCPP_INFO(this->get_logger(), "大家好，我是得了穷病的张三.");
         client_ = this->create_client<village_interfaces::srv::SellNovel>("sell_novels");
     }
+
+    void buy_novel(){
+        RCLCPP_INFO(this->get_logger(), "买小说去喽");
+        village_interfaces::srv::SellNovel_Request request;
+        request.money = 10;
+        while(!client_->wait_for_service(std::chrono::seconds(1))&&rclcpp::ok()){
+            RCLCPP_INFO(this->get_logger(), "等待服务端上线中");
+        }
+    };
+
 
 private:
     rclcpp::CallbackGroup::SharedPtr callback_group_subscriber1_;
@@ -35,6 +43,7 @@ int main(int argc, char **argv)
     /* 运行节点，并检测退出信号*/
     rclcpp::executors::MultiThreadedExecutor exector;
     exector.add_node(node);
+    node->buy_novel();
     exector.spin();
     rclcpp::shutdown();
     return 0;
