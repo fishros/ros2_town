2656bac2d321b22a123b27dd3d327f77086a9ec5
aiotrobot <sangxin2014@sina.com>
2021-09-17 02:12:12 -0700

    modify li4 node

--- village_li/village_li/li4.py
+++ village_li/village_li/li4.py
@@ -9,5 +9,5 @@ def main(args=None):
     rclpy.init(args=args) # 初始化rclpy
     node = Node("li4")  # 新建一个节点
     node.get_logger().info("Hello I am li4!")
-    # rclpy.spin(node) # 保持节点运行，检测是否收到退出指令（Ctrl+C）
+    rclpy.spin(node) # 保持节点运行，检测是否收到退出指令（Ctrl+C）
     rclpy.shutdown() # rcl关闭
